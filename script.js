89c94a5f514486fd23d66d423dbe26711cf54804
Esen Espinosa Cook <esen.espinosa@intercorpretail.pe>
2020-09-17 02:56:02 -0500

    new commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//https://public-api.wordpress.com/wp/v2/sites/175798736/posts
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    posts: {},
+    imvisible: true,
+    imurl: "https://theworldofnoth.files.wordpress.com/2020/06/default_men.png",
+    imtext: "Placeholder text",
+  }, 
+  mounted(){
+    this.fetchPosts()
+  },
+  methods: {
+      async fetchPosts(){
+        // GET request using fetch with async/await
+        const response = await fetch("https://public-api.wordpress.com/wp/v2/sites/175798736/posts?categories=395261")
+        const data = await response.json()
+        this.posts = data
+      },
+      showModal(post){
+          this.imurl = post.jetpack_featured_media_url
+          this.imtext = post.content["rendered"]
+          this.imvisible = true
+      }
+  }
+})
