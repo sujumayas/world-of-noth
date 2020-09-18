49d8e79b6e3729d6faadd254cc1385551bca8979
Esen Espinosa Cook <esen.espinosa@intercorpretail.pe>
2020-09-17 22:32:49 -0500

    some tests with categories

---
+++ modules/config.js
@@ -0,0 +1,5 @@
+export const _CONFIG = {
+    appName: "The World of Noth",
+    //apiBaseURL: "https://public-api.wordpress.com/wp/v2/sites/175798736",
+    apiBaseURL:"https://public-api.wordpress.com/rest/v1.1/sites/theworldofnoth.wordpress.com",
+}
--- script.js
+++ script.js
@@ -1,24 +1,59 @@
 //https://public-api.wordpress.com/wp/v2/sites/175798736/posts
-
+import { _CONFIG } from './modules/config.js'
 
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!',
     posts: {},
+    showingCategory: "",
+    categories: [],
     imvisible: false,
     imurl: "https://theworldofnoth.files.wordpress.com/2020/06/default_men.png",
     imtext: "Placeholder text",
   }, 
   mounted(){
-    this.fetchPosts()
+    console.log("config loaded...")
+    this.fetchInitialPosts()
+    this.fetchCategories()
+  },
+  computed : {
+    currentPosts : function(){
+      return this.posts[this.showingCategory]
+    }
   },
   methods: {
-      async fetchPosts(){
+      async fetchInitialPosts(){
         // GET request using fetch with async/await
-        const response = await fetch("https://public-api.wordpress.com/wp/v2/sites/175798736/posts?categories=395261")
+        const response = await fetch(_CONFIG.apiBaseURL+"/posts?category=dragones")
         const data = await response.json()
-        this.posts = data
+        this.posts["dragones"] = data.posts
+        this.showingCategory = "dragones"
+        console.log(`Found ${data.found} posts`)
+        //console.log(this.posts)
+      },
+      async fetchCategories(){
+        // GET request using fetch with async/await
+        const response = await fetch(_CONFIG.apiBaseURL+"/categories")
+        const data = await response.json()
+        let vmCategoryList = this.categories
+        data.categories.map(function(category){
+          let {ID, name, post_count, parent, slug} = category
+          vmCategoryList.push({ID, name, post_count, parent, slug}) 
+        })
+        console.log(`Found ${data.found} categories`)
+        console.log(this.categories)
+      },
+      async reloadCharacters(slug){
+        // GET request using fetch with async/await
+        if(!this.posts[slug]){
+          const response = await fetch(_CONFIG.apiBaseURL+"/posts?category="+slug)
+          const data = await response.json()
+          this.posts[slug] = data.posts
+        }
+        this.showingCategory = slug
+        console.log(`Found ${data.found} posts for ${slug} category`)
+        //console.log(this.posts)
       },
       showModal(post){
           this.imurl = post.jetpack_featured_media_url
